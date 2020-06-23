4b6f8cecff8a03b8acdb90b9b42883ff83c821d4
Felipe Pereira <felipepereira@acad.ifma.edu.br>
2020-06-23 09:39:34 -0300

    Revert "organizando"
    This reverts commit f1b6b4f9b50a187927c7e75c749db53cfc5f1715.

---
+++ 01-3-empresa.js
@@ -0,0 +1,16 @@
+const usuario = {
+    nome: "Diego",
+    empresa: {
+        nome: "Rocketseat",
+        cor: "Roxo",
+        foco: "Programação",
+        endereco: {
+            rua: "Rua Guilherme Gembala",
+            numero: 260
+
+
+        }
+    }
+}
+
+console.log(`A empresa ${usuario.empresa.nome} está localizada na ${usuario.empresa.endereco.rua}, Nº ${usuario.empresa.endereco.numero}`)
