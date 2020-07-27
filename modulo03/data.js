6cbf42a7bb3fa294b707f8f1181afa9521b5378b
Felipe Pereira <felipepereira@acad.ifma.edu.br>
2020-07-27 10:44:08 -0300

    iniciando back-end

---
+++ modulo03/data.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        id: "Starter.64c237cc",
+        title: "Torne-se um programador desejado no mercado com esses cursos gratuitos"
+
+    },
+    {
+        id: "LaunchBase.08285320",
+        title: "Domine programação do zero e tenha acesso as melhores oportunidade do mercado",
+
+    }, 
+    {
+        id: "GoStack.83a178a0",
+        title: "Treinamento imersivo nas tecnologias mais modernas de desenvolvimento web e mobile"
+
+    }
+    
+]
